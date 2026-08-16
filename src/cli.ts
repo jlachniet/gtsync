4dbec3f5c35c860a2f6d0702204fb23905801699
Julian Lachniet <julian@lachniet.com>
2026-08-16 19:02:24 -0400

    feat: Set up a dummy CLI and fix dist not being deleted before builds

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,10 @@
-console.info('Hello, World!');
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+	.argument('<source-path>', 'The path to the source audio file')
+	.argument('<target-path>', 'The path to the target audio file')
+	.argument('<output-path>', 'The path to the output audio file');
+
+program.parse();
